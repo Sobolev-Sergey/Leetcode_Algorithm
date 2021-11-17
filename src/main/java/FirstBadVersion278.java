bd21e2353c4ed5f1a3233fe757428137a4ed384b
Auriga <Auriga@DESKTOP-RTE78EB.office.auriga.msk>
2021-11-17 12:35:32 +0300

    Task 704, 278, 35

---
+++ src/main/java/FirstBadVersion278.java
@@ -0,0 +1,99 @@
+package main.java;
+
+public class FirstBadVersion278 {
+    /**
+     * 278. First Bad Version
+     * You are a product manager and currently leading a team to develop a new product. Unfortunately,
+     * the latest version of your product fails the quality check. Since each version is developed based on
+     * the previous version, all the versions after a bad version are also bad.
+     *
+     * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all
+     * the following ones to be bad.
+     *
+     * You are given an API bool isBadVersion(version) which returns whether version is bad.
+     * Implement a function to find the first bad version. You should minimize the number of calls to the API.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: n = 5, bad = 4
+     * Output: 4
+     * Explanation:
+     * call isBadVersion(3) -> false
+     * call isBadVersion(5) -> true
+     * call isBadVersion(4) -> true
+     * Then 4 is the first bad version.
+     * Example 2:
+     *
+     * Input: n = 1, bad = 1
+     * Output: 1
+     */
+
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+    public class Solution extends VersionControl {
+        public int firstBadVersion(int n) { // 10 bad =7
+            int left= 0;
+            int right = n;
+
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (isBadVersion(mid)) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        }
+    }
+
+
+    int firstBadVersion (int n ){
+        if(n == 1) return n;
+
+        int left = 1;
+        int right = n;
+        int mid;
+        int result;
+
+        while(left <= right){
+            mid = left + ((right - left)/2);
+            if(isBadVersion(mid)){
+                result = mid;
+                right = mid - 1;
+            } else{
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+
+
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+    public class Solution extends VersionControl {
+        public int firstBadVersion (int n ){
+            if(n == 1) return n;
+
+            int left = 1;
+            int right = n;
+            int mid;
+            int result = 0;
+
+            while(left <= right){
+                mid = left + ((right - left)/2);
+                if(isBadVersion(mid)){
+                    result = mid;
+                    right = mid - 1;
+                } else{
+                    left = mid + 1;
+                }
+            }
+            return result;
+        }
+    }
+}
